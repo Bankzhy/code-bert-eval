a8bf0eb0e0e52279d053631c863aa4b100caa059
bankzhy <3532553868@qq.com>
2023-11-20 17:31:31 +0800

    fix sh1

--- summarization/cb/run.sh
+++ summarization/cb/run.sh
@@ -4,12 +4,12 @@ batch_size=64
 beam_size=10
 source_length=256
 target_length=128
-data_dir=../dataset
+data_dir=../../dataset
 output_dir=model/$lang
 train_file=$data_dir/tl/train
 dev_file=$data_dir/tl/valid
-eval_steps=1000 #400 for ruby, 600 for javascript, 1000 for others
-train_steps=50000 #20000 for ruby, 30000 for javascript, 50000 for others
+eval_steps=1 #400 for ruby, 600 for javascript, 1000 for others
+train_steps=5 #20000 for ruby, 30000 for javascript, 50000 for others
 pretrained_model=microsoft/codebert-base #Roberta: roberta-base
 
 python run.py --do_train --do_eval --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --dev_filename $dev_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --train_steps $train_steps --eval_steps $eval_steps
---
+++ summarization/spt/run_spt.sh
@@ -0,0 +1,8 @@
+python run_spt.py \
+--do-fine-tune \
+--task summarization \
+--summarization-language java \
+--model-name summarization_java \
+--n_epoch 50 \
+--batch_size 32 \
+--max_nl_len 128
