ef67978a0eb884d8224d38ccd266bdfef7f5349a
bankzhy <3532553868@qq.com>
2023-11-23 09:14:51 +0800

    fix gc9

---
+++ summarization/gcb/run_test.sh
@@ -0,0 +1,26 @@
+source=java
+pretrained_model=microsoft/graphcodebert-base
+batch_size=64
+output_dir=saved_models/$source/
+dev_file=../dataset/tl/valid
+test_file=../dataset/tl/test
+load_model_path=$output_dir/checkpoint-best-bleu/pytorch_model.bin #checkpoint for test
+source_length=256
+target_length=128
+beam_size=10
+
+python run.py \
+--do_test \
+--model_type roberta \
+--source_lang $source \
+--model_name_or_path $pretrained_model \
+--tokenizer_name microsoft/graphcodebert-base \
+--config_name microsoft/graphcodebert-base \
+--load_model_path $load_model_path \
+--dev_filename $dev_file \
+--test_filename $test_file \
+--output_dir $output_dir \
+--max_source_length $source_length \
+--max_target_length $target_length \
+--beam_size $beam_size \
+--eval_batch_size $batch_size 2>&1| tee $output_dir/test.log
